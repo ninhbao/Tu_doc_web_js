5474c98671f013dbd6dae6b50c6175fdf8fb7112
Ninh Floder <ngobaoninh1211@gmail.com>
2024-10-17 20:33:38 +0700

    Update

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-alert("Xin chào đây là một thông báo từ javascript");
-alert("Tôi là Ngô Bảo Ninh");
-alert("Thiết kế web bán hàng phân bón");
+// alert("Bao Ninh Agricare");
+
+//Hiển thị hộp nhập liệu
+// prompt("Nhập vào tên của bạn:");
+//khai báo biến
+var myName;
+let yourName;
+
+//khởi tạo biến
+var diemToan = 10;
+const doSoi = 100;
+let firtName = "Alice";
+
+//xuất giá trị của biến
+console.log("Điêm toán:" + diemToan);
+console.log("Độ sôi " + doSoi + " độ C");
+
+firtName = "Ninh";
+console.log(firtName);
+
+//Nhập và xuất tên nguời dùng
+myName = prompt("Nhập tên của bạn: ");
+console.log("Tên của bạn là: " + myName);
